2ef0c409cfe415d9e825bdea439becda150aa17d
Sid Hays <sidh@solution-source.net>
2020-12-03 10:32:59 -0500

    Sync. I honestly don't remember what I was doing in this commit.

--- users/ozzzzysh/ozzzzysh.c
+++ users/ozzzzysh/ozzzzysh.c
@@ -5,6 +5,39 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+__attribute__ ((weak))
+bool matrix_scan_keymap(void) {
+  return true;
+}
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        // SEQ_TWO_KEYS(KC_LALT, KC_4) {
+        //     // SS_LALT();
+        //     SEND_STRING(SS_DOWN(KC_LALT));
+        //     SEND_STRING(SS_TAP(KC_F4));
+        //     SEND_STRING(SS_UP(KC_LALT));
+        // }
+        // SEQ_TWO_KEYS(KC_D, KC_D) {
+        //     SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
+        // }
+        // SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
+        //     SEND_STRING("https://start.duckduckgo.com\n");
+        // }
+        // SEQ_TWO_KEYS(KC_A, KC_S) {
+        //     register_code(KC_LGUI);
+        //     register_code(KC_S);
+        //     unregister_code(KC_S);
+        //     unregister_code(KC_LGUI);
+        // }
+    }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_MAKE:  // Compiles the firmware, and adds the flash command based on keyboard bootloader
